83b1c6f908de61a2a66c185fed3aeac1f3583488
splach-coder <anasbenabbou776@gmail.com>
2022-12-05 23:41:31 +0100

    finished the filter in the chart

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 $(document).ready(function () {
     const ctx = document.getElementById('myChart');
+    var dataShowedUp = false;
+    var currentCountry;
     var myChart;
     var labels = [],
         Confirmed = [],
@@ -22,8 +24,10 @@ $(document).ready(function () {
                 });
 
                 $(".country").click(function () {
-                    $("#title").text($(this).text() + "'s Data");
-                    $.get("https://api.covid19api.com/dayone/country/" + $(this).data('slug'))
+                    currentCountry = $(this).text();
+                    $("#title").text(currentCountry + "'s Data");
+
+                    $.get("https://api.covid19api.com/dayone/country/" + currentCountry)
                         .done(function (data) {
                             var year = $("#year option:selected").val();
                             var part = $("#part option:selected").val();
@@ -95,7 +99,7 @@ $(document).ready(function () {
 
                             myChart = setChart(myChart, ctx, myData, 'update');
 
-                            console.log('kmlat')
+                            dataShowedUp = true;
                         });
                 })
             });
@@ -103,8 +107,102 @@ $(document).ready(function () {
 
 
     $("#year").on('change', function () {
-        /*do some here */
+        if (dataShowedUp)
+            showRefrechBtn(true);
     })
+
+    $("#part").on('change', function () {
+        if (dataShowedUp)
+            showRefrechBtn(true);
+    })
+
+    $(".Refresh-btn").click(function () {
+        showRefrechBtn(false);
+
+        $.get("https://api.covid19api.com/dayone/country/" + currentCountry)
+            .done(function (data) {
+                var year = $("#year option:selected").val();
+                var part = $("#part option:selected").val();
+                var season;
+
+                data.forEach(e => {
+                    let date = new Date(e.Date);
+
+                    if (date.getFullYear() == year) {
+                        switch (part) {
+                            case 's1':
+                                season = [1, 2, 3];
+                                break;
+                            case 's2':
+                                season = [4, 5, 6];
+                                break;
+                            case 's3':
+                                season = [7, 8, 9];
+                                break;
+                            case 's4':
+                                season = [10, 11, 12];
+                                break;
+                            default:
+                                //
+                        }
+
+                        if (season.includes(date.getMonth() + 1)) {
+                            labels.push(e.Date.slice(0, 10));
+                            Confirmed.push(e.Confirmed);
+                            Deaths.push(e.Deaths);
+                            Recovered.push(e.Recovered);
+                            Active.push(e.Active);
+                        }
+                    }
+                })
+
+                myData = {
+                    labels: labels,
+                    datasets: [{
+                            label: "Actives",
+                            fill: false,
+                            backgroundColor: 'rgba(95, 0, 186, .25)',
+                            borderColor: 'rgb(95, 0, 186)',
+                            data: Active,
+                        },
+                        {
+                            label: "Confirmed",
+                            fill: false,
+                            backgroundColor: 'rgba(137, 189, 158, .25)',
+                            borderColor: 'rgb(137, 189, 158)',
+                            data: Confirmed,
+                        },
+                        {
+                            label: "Deaths",
+                            fill: false,
+                            backgroundColor: 'rgba(233, 79, 55, .25)',
+                            borderColor: 'rgb(233, 79, 55)',
+                            data: Deaths,
+                        },
+                        {
+                            label: "Recovered",
+                            fill: false,
+                            backgroundColor: 'rgba(25, 123, 189, 0.25)',
+                            borderColor: 'rgb(25, 123, 189)',
+                            data: Recovered
+                        }
+                    ]
+                };
+
+                myChart = setChart(myChart, ctx, myData, 'update');
+
+                dataShowedUp = true;
+            });
+
+    })
+
+
+    function showRefrechBtn(visibility) {
+        if (visibility)
+            $(".Refresh-btn").css('scale', '1');
+        else
+            $(".Refresh-btn").css('scale', '0');
+    }
 
     function init(ctx) {
         getcountries();
