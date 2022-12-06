dd87cd0f82c540ffb40e6c067eb93316c7e5bda9
splach-coder <anasbenabbou776@gmail.com>
2022-12-06 11:01:28 +0100

    delete some useless funcs

--- script.js
+++ script.js
@@ -278,14 +278,4 @@ $(document).ready(function () {
 
         return myChart;
     }
-
-    function addData(chart, label, data) {
-        chart.data.labels.push(label);
-        chart.data.datasets.forEach((dataset) => {
-            dataset.data.push(data);
-        });
-        chart.update();
-    }
-
-
 })
